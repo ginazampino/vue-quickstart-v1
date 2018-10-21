228576fd0dbefb9f90f5201f053433d8648c0752
Gina Zampino <zampino.gina@gmail.com>
2018-10-20 20:58:16 -0400

    Initial commit.

---
+++ front_end/webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const isDevMode = process.env.NODE_ENV === 'development';
+
+module.exports = {
+    mode: isDevMode ? 'development' : 'production',
+
+    entry: {
+        'main': [
+            'babel-polyfill',
+            path.join(__dirname, 'index')
+        ]
+    },
+
+    output: {
+        filename: isDevMode ? '[name].js' : '[name].[hash].js',
+        path: path.resolve(__dirname, '../wwwroot')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    { loader: isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
+                ]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|tiff|bmp|webm)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'images/[name].[ext]'
+                }
+            }
+        ]
+    },
+
+    optimization: {
+        minimizer: [      
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname, 'index.html'),
+            chunks: ['main']
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: isDevMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevMode ? '[id].css' : '[id].[hash].css',
+        }),
+
+        new VueLoaderPlugin()
+    ],
+
+    resolve: {
+        alias: {
+            '~': __dirname,
+            'root': __dirname,
+            'vue': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['.json', '.js', '.css', '.scss']
+    },
+
+    devtool: isDevMode ? 'cheap-module-eval-source-map' : 'source-map'
+};
